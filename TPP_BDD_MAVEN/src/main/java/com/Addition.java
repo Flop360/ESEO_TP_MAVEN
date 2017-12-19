2de60ab5f6036fb3cd32b3f552e8ca2a419bac75
florent <florent@FloKali>
2017-12-19 14:42:12 +0100

    test

--- TPP_BDD_MAVEN/src/main/java/com/Addition.java
+++ TPP_BDD_MAVEN/src/main/java/com/Addition.java
@@ -2,7 +2,7 @@ package com;
 
 public class Addition {
 	public static long calculer(long a, long b) {
-		return a+b+1;
+		return a+b+1+3;
 	}
 	public static Character lireSymbole() {
 		return '-';
