b26e52978357b117ae77e4eea02cfd9992d84407
Florent <florent.dupe@reseau.eseo.fr>
2018-01-16 14:48:09 +0100

    bla

--- TPP_BDD_MAVEN/src/main/java/com/ApplicationBDD.java
+++ TPP_BDD_MAVEN/src/main/java/com/ApplicationBDD.java
@@ -1,5 +1,97 @@
 package com;
 
-public class ApplicationBDD {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+import com.bean.*;
+import com.blo.VilleBLOImpl;
+import com.controller.VilleController;
+import com.dao.VilleDAOImpl;
 
+public class ApplicationBDD {
+	
+	private static final String FICHIER_CSV = "laposte_hexasmal.csv";
+	private static final String FICHIER_INFOS_PROPERTIES = "info.properties";
+	private static final String PROPERTY_VERSION = "version";
+	private static final String PROPERTY_PLATEFORME = "plateforme";
+	
+	public static void main( String[] args )
+    {
+    	Properties properties = new Properties();
+    	Properties properties1 =new Properties();
+        String version;
+        String plateforme;
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream fichierInfoProperties = classLoader.getResourceAsStream(FICHIER_INFOS_PROPERTIES);
+		InputStream fichierCSV = classLoader.getResourceAsStream(FICHIER_CSV);
+		
+		
+		try {
+			properties.load(fichierInfoProperties);
+			properties1.load(fichierCSV);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		version = properties.getProperty(PROPERTY_VERSION);
+		plateforme = properties.getProperty(PROPERTY_PLATEFORME);
+		
+		
+		
+		ArrayList<Ville> listVilles = new ArrayList<Ville>();
+		ArrayList<Ville> listVillesDao = new ArrayList<Ville>();
+		
+		
+		VilleBLOImpl villeBloImpl = new VilleBLOImpl();
+		VilleDAOImpl villeDaoImpl = new VilleDAOImpl();
+		
+		VilleController vc = new VilleController();
+		
+		listVilles = villeBloImpl.getVilles();
+		
+		listVillesDao = villeDaoImpl.trouverVilles();
+		
+		vc.synchroniseVilles(listVilles, listVillesDao);
+		
+		
+		
+		String nom_commune;
+		String code_postal;
+		String code_commune;
+		String ligne5;
+		String libelle;
+		String latitude;
+		String longitude;
+		
+		System.out.println( "Hello World!" );
+		System.out.println("\n-----------------------------------------------------");
+		System.out.println("la version de l'application est : "+version);
+		System.out.println("la platerforme de l'application est : "+plateforme);
+		System.out.println("Voici le résultat du fichier CSV. Suite à ce fichiers, la base de données est en cours de mise à jour !");
+		for (Ville ville : listVilles) {
+			code_commune = ville.getCode_commune_INSEE();
+			code_postal = ville.getCode_postal();
+			libelle = ville.getLibelle_acheminement();
+			ligne5 = ville.getLigne_5();
+			nom_commune = ville.getNom_commune();
+			latitude = ville.getCoordonnees_gps().getLatitude();
+			longitude = ville.getCoordonnees_gps().getLongitude();
+			
+			System.out.println(" Code Commune : "+code_commune+ " ; Nom Commune : "+nom_commune+" ; Code Postal : "
+					+code_postal+" ; Libellé : "+libelle+" ; Ligne 5 : "+ligne5+" ; Latitude : "+latitude+" ; Longitude : "
+					+longitude);
+			
+		}
+		
+		
+		
+		
+		
+	
+		Connect.main();
+        
+    }
+	
 }
--- TPP_BDD_MAVEN/src/main/java/com/controller/VilleController.java
+++ TPP_BDD_MAVEN/src/main/java/com/controller/VilleController.java
@@ -12,6 +12,21 @@ public class VilleController {
 	VilleBLO villesBLO;
 	VilleDAO villesDAO;
 	
+	public ArrayList<Ville> synchroniseVilles(ArrayList<Ville> listVilleBlo, ArrayList<Ville> listVilleDao){
+		
+		
+		listVilleBlo = villesBLO.getVilles();
+		listVilleDao = villesDAO.trouverVilles();
+		
+		listVilleBlo.removeAll(listVilleDao);
+		
+		villesDAO.ajouterVilles(listVilleBlo);
+		
+		return listVilleBlo;
+		
+		
+	}
+	
 	public void synchroniseVilles() {
 		ArrayList<Ville> listVilleBlo = new ArrayList<Ville>();
 		ArrayList<Ville> listVilleDao = new ArrayList<Ville>();
