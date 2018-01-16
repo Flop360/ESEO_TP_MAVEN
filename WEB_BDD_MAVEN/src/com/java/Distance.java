088a818659e79030371111f86f03f667fe74ad7d
Ludwig <ludwig.courousse@reseau.eseo.fr>
2018-01-16 10:37:58 +0100

    Distance

--- WEB_BDD_MAVEN/src/com/java/Distance.java
+++ WEB_BDD_MAVEN/src/com/java/Distance.java
@@ -8,11 +8,7 @@ public class Distance {
 	public static String getDistance(Ville ville1, Ville ville2) {
 		
 		String latAold = ville1.getCoordonnees_gps().getLatitude();
-		System.out.println(latAold);
-		//String latANew = latAold.replace(".",",");
-		//System.out.println(latANew);
 		float latA = Float.valueOf(latAold);
-		System.out.println(latA);
 		float longA = Float.valueOf(ville1.getCoordonnees_gps().getLongitude());
 		float latB = Float.valueOf(ville2.getCoordonnees_gps().getLatitude());
 		float longB = Float.valueOf(ville2.getCoordonnees_gps().getLongitude());
@@ -20,14 +16,15 @@ public class Distance {
 		int rayon = 6371;
 		
 		
-		int radlatA = (int) ((latA*Math.PI)/180);
-		int radlongA = (int) ((longA*Math.PI)/180);
+		float radlatA = (float) ((latA*Math.PI)/180);
+		float radlongA = (float) ((longA*Math.PI)/180);
 		
-		int radlatB = (int) ((latB*Math.PI)/180);
-		int radlongB = (int) ((longB*Math.PI)/180);
+		float radlatB = (float) ((latB*Math.PI)/180);
+		float radlongB = (float) ((longB*Math.PI)/180);
 		
-		int distance = (int) (rayon * Math.acos(Math.sin(radlatA)*Math.sin(radlatB) 
+		float distance = (float) (rayon * Math.acos(Math.sin(radlatA)*Math.sin(radlatB) 
 				+ Math.cos(radlatA)*Math.cos(radlatB)*Math.cos(radlongA-radlongB)));
+		
 		String distanceValue = String.valueOf(distance);
 		
 		return distanceValue;
--- WEB_BDD_MAVEN/testa/com/java/DistanceTest.java
+++ WEB_BDD_MAVEN/testa/com/java/DistanceTest.java
@@ -1,19 +1,23 @@
 package com.java;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import com.bean.*;
 public class DistanceTest {
 	
 	@Test
 	public void getDistanceTest() {
-		CoordonneesGPS coordGPS1 = new CoordonneesGPS("48.4916267047", "3.93350100384");
-		Ville ville1 = new Ville("10166","LES GRANDES CHAPELLES", "LES GRANDES CHAPELLES","10170","",coordGPS1);
+		CoordonneesGPS coordGPS1 = new CoordonneesGPS("50.62925", "3.057256");
+		Ville ville1 = new Ville("","Lille", "","","",coordGPS1);
 		
 		CoordonneesGPS coordGPS2 = new CoordonneesGPS("43.4833300","-1.4833300");
-		Ville ville2 = new Ville("", "", "", "", "", coordGPS2);
+		Ville ville2 = new Ville("", "Bayonne", "", "", "", coordGPS2);
 		
 		String distance = Distance.getDistance(ville1, ville2);
 		System.out.println(distance);
+		
+		assertEquals("Should be equals : ", "865.431",distance);
 		
 		
 		
